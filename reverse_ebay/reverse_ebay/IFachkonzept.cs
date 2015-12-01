5ef4e280731ba9ba9464a1409f00c7134563d16d
Exturium <ghostreapersworld@gmail.com>
2015-12-01 21:25:02 +0100

    Commit

--- reverse_ebay/reverse_ebay/IFachkonzept.cs
+++ reverse_ebay/reverse_ebay/IFachkonzept.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace reverse_ebay
 {
-    abstract class IFachkonzept
+    interface IFachkonzept
     {
+        // Benutzer-Management
+        Boolean erzeugeBenutzer(string name, string passwort);
+        Boolean aendereBenutzer(string name, string passwort);
+        Boolean loescheBenutzer(int id);
+        Benutzer gibBenutzer(int id);
+        Boolean einloggen();
+        Boolean ausloggen();
+        List<Adresse> meineAdressen();
+        List<Artikel> meineArtikel();
+
+        // Adressen-Management
+        Boolean erzeugeAdresse(string str_nr, int plz, string stadt, string land);
+        Boolean aendereAdresse(string str_nr, int plz, string stadt, string land);
+        Boolean loescheAdresse(int id);
+
+        // Artikel-Management
+        Boolean erzeugeArtikel(string name, string kurzbeschr, string langbeschr, int anbieter_id, int bieter_id, DateTime ablaufdatum, int gebot);
+        Boolean aendereArtikel(string name, string kurzbeschr, string langbeschr, int anbieter_id, int bieter_id, DateTime ablaufdatum, int gebot);
+        Boolean loescheArtikel(int id);
+        Artikel gibArtikel(int id);
+
+        // Sonstige Funktionen
+        Boolean aufArtikelBieten(int gebot);
+        List<Artikel> meineGeboteAnzeigen();
+        Boolean istArtikelAktiv(int id);
+        List<Artikel> sucheArtikel(string name_suchstring, string kurzbeschr_suchstring, string langbeschr_suchstring);
+
     }
 }
+
