67f6ece45d98927b0443a77b44b04f21f56eeffe
Exturium <ghostreapersworld@gmail.com>
2016-01-16 19:58:39 +0100

    Fachkonzept 2

---
+++ reverse_ebay/reverse_ebay/Fachkonzept/Fachkonzept2/Fachkonzept2.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_ebay
+{
+    class Fachkonzept2
+    {
+        private ReverseEbayFacade reverseEbayFacade;
+
+        public Fachkonzept2(IDatenhaltung _datenhaltung)
+        {
+            this.reverseEbayFacade = new ReverseEbayFacade(_datenhaltung);
+        }
+
+        // Ausgabe des aktuellen Nutzers
+        public Benutzer gibAktBenutzer()
+        {
+            return reverseEbayFacade.gibAktBenutzer();
+        }
+
+
+        // Benutzer-Management
+        public bool erzeugeBenutzer(Benutzer benutzer)
+        {
+            return reverseEbayFacade.erzeugeBenutzer(benutzer);
+        }
+        public bool aendereBenutzer(Benutzer benutzer)
+        {
+            return reverseEbayFacade.aendereBenutzer(benutzer);
+        }
+        public bool loescheBenutzer(Benutzer benutzer)
+        {
+            return reverseEbayFacade.loescheBenutzer(benutzer);
+        }
+        public Benutzer gibBenutzer(int id)
+        {
+            try
+            {
+                foreach(Benutzer benutzer in reverseEbayFacade.gibAlleBenutzerListe())
+                {
+                    if (benutzer.id == id)
+                    {
+                        return benutzer;
+                    }
+                }
+                return null;
+            }
+            catch { return null; }
+        }
+        public bool einloggen(string name, string passwort)
+        {
+            Benutzer tempBenutzer = new Benutzer();
+            tempBenutzer.name = name;
+            tempBenutzer.passwort = passwort;
+            return reverseEbayFacade.oeffneBenutzer(tempBenutzer);
+        }
+        public bool ausloggen()
+        {
+            return reverseEbayFacade.schliesseBenutzer();
+        }
+        public List<BenutzerAdresse> meineAdressen()
+        {
+            return reverseEbayFacade.gibAktBenutzerAdressenListe();
+        }
+        public List<Artikel> meineArtikel(bool nuroffen)
+        {
+            List<Artikel> tempArtikelListe = reverseEbayFacade.gibAktBenutzerArtikelListe();
+            List<Artikel> benutzerArtikelListe = new List<Artikel>();
+           
+            if (nuroffen)
+            {
+                foreach (Artikel artikel in tempArtikelListe)
+                {
+                    if (artikel.ablaufdatum < DateTime.Now)
+                    {
+                        benutzerArtikelListe.Add(artikel);
+                    }
+                }
+            }
+            else
+            {
+                benutzerArtikelListe = tempArtikelListe;
+            }
+
+            return benutzerArtikelListe;
+        }
+
+
+        // BenutzerAdressen-Management
+        public bool erzeugeBenutzerAdresse(BenutzerAdresse benutzeradresse)
+        {
+            return reverseEbayFacade.erzeugeBenutzerAdresse(benutzeradresse);
+        }
+        public bool aendereBenutzerAdresse(BenutzerAdresse benutzeradresse)
+        {
+            return reverseEbayFacade.aendereBenutzerAdresse(benutzeradresse);
+        }
+        public bool loescheBenutzerAdresse(BenutzerAdresse benutzeradresse)
+        {
+            return reverseEbayFacade.loescheBenutzerAdresse(benutzeradresse);
+        }
+
+
+        // Artikel-Management
+        public bool erzeugeArtikel(Artikel artikel)
+        {
+            return reverseEbayFacade.erzeugeBenutzerArtikel(artikel);
+        }
+        public bool aendereArtikel(Artikel artikel)
+        {
+            return reverseEbayFacade.aendereBenutzerArtikel(artikel);
+        }
+        public bool loescheArtikel(Artikel artikel)
+        {
+            return reverseEbayFacade.loescheBenutzerArtikel(artikel);
+        }
+        public Artikel gibArtikel(int id)
+        {
+            try
+            {
+                foreach (Artikel artikel in reverseEbayFacade.gibAlleArtikelListe())
+                {
+                    if (artikel.id == id)
+                    {
+                        return artikel;
+                    }
+                }
+                return null;
+            }
+            catch { return null; }
+        }
+
+
+        // Sonstige Funktionen
+        public bool aufArtikelBieten(Artikel artikel, double gebot)
+        {
+
+        }
+        public List<Artikel> meineGeboteAnzeigen()
+        {
+            List<Artikel> artikelListe = datenhaltung.getItemList();
+            List<Artikel> meineGeboteListe = new List<Artikel>();
+            foreach (Artikel artikel in artikelListe)
+            {
+                if (artikel.bieter_id == aktBenutzer.id)
+                {
+                    meineGeboteListe.Add(artikel);
+                }
+            }
+            return meineGeboteListe;
+        }
+        public bool istArtikelAktiv(Artikel artikel)
+        {
+            List<Artikel> artikelListe = datenhaltung.getItemList();
+            foreach (Artikel art in artikelListe)
+            {
+                if (art.id == artikel.id && art.ablaufdatum > DateTime.Now)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public List<Artikel> gibArtikelListe(string suchstring = "")
+        {
+            // Derzeit ist nur eine Volltextsuch auf die Kurzbeschreibung möglich
+            List<Artikel> artikelListe = datenhaltung.getItemList();
+            List<Artikel> meineArtikelListe = new List<Artikel>();
+            if ((suchstring != "") && (artikelListe.Count > 0))
+            {
+                foreach (Artikel artikel in artikelListe)
+                {
+                    if (artikel.kurzbeschr == suchstring)
+                    {
+                        meineArtikelListe.Add(artikel);
+                    }
+                }
+                return meineArtikelListe;
+            }
+            else
+            {
+                return artikelListe;
+            }
+        }
+    }
+}
---
+++ reverse_ebay/reverse_ebay/Fachkonzept/Fachkonzept2/ReverseEbayFacade.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_ebay
+{
+    class ReverseEbayFacade
+    {
+        private IDatenhaltung datenhaltung;
+
+        private Benutzer aktBenutzer;
+        private List<Artikel> aktBenutzer_ArtikelListe;
+        private List<Artikel> alleArtikelListe;
+        private List<Benutzer> alleBenutzerListe;
+        private List<BenutzerAdresse> aktBenutzer_AdressenListe;
+
+        public ReverseEbayFacade(IDatenhaltung _datenhaltung)
+        {
+            this.datenhaltung = _datenhaltung;
+            this.aktBenutzer_ArtikelListe = new List<Artikel>();
+            this.alleArtikelListe = new List<Artikel>();
+            this.alleBenutzerListe = new List<Benutzer>();
+            this.aktBenutzer_AdressenListe = new List<BenutzerAdresse>();
+        }
+
+        // Laden / Entladen der Facade
+        public bool oeffneBenutzer(Benutzer benutzer)
+        {
+            try
+            {
+                Benutzer tempBenutzer = datenhaltung.getUser(benutzer.name);
+                if (tempBenutzer.passwort == benutzer.passwort)
+                {
+                    aktBenutzer = tempBenutzer;
+
+                    List<Artikel> tempArtikelListe = datenhaltung.getItemList();
+                    foreach (Artikel artikel in tempArtikelListe)
+                    {
+                        if (artikel.anbieter_id == aktBenutzer.id)
+                        {
+                            aktBenutzer_ArtikelListe.Add(artikel);
+                        }
+                    }
+                    alleArtikelListe = tempArtikelListe;
+
+                    List<BenutzerAdresse> tempBenutzerAdressenListe = datenhaltung.getUserAdressList();
+                    foreach (BenutzerAdresse benutzerAdresse in tempBenutzerAdressenListe)
+                    {
+                        if (benutzerAdresse.benutzer_id == aktBenutzer.id)
+                        {
+                            aktBenutzer_AdressenListe.Add(benutzerAdresse);
+                        }
+                    }
+
+                    return true;
+                }
+                return false;
+            } catch { return false; }
+        }
+        public bool schliesseBenutzer()
+        {
+            try
+            {
+                aktBenutzer = null;
+                aktBenutzer_ArtikelListe = new List<Artikel>();
+                aktBenutzer_AdressenListe = new List<BenutzerAdresse>();
+                return true;
+            }
+            catch { return false; }
+        }
+        private void aktualisiereBenutzerdaten()
+        {
+            Benutzer tempBenutzer = aktBenutzer;
+            schliesseBenutzer();
+            oeffneBenutzer(tempBenutzer);
+        }
+
+        // Zugriffsfunktionen
+        public Benutzer gibAktBenutzer()
+        {
+            return aktBenutzer;
+        }
+        public List<Artikel> gibAktBenutzerArtikelListe()
+        {
+            return aktBenutzer_ArtikelListe;
+        }
+        public List<Artikel> gibAlleArtikelListe()
+        {
+            return alleArtikelListe;
+        }
+        public List<Benutzer> gibAlleBenutzerListe()
+        {
+            return alleBenutzerListe;
+        }
+        public List<BenutzerAdresse> gibAktBenutzerAdressenListe()
+        {
+            return aktBenutzer_AdressenListe;
+        }
+
+        // Benutzer-Management
+        public bool erzeugeBenutzer(Benutzer benutzer)
+        {
+            try
+            {
+                if (datenhaltung.insertUser(benutzer.name, benutzer.passwort) != 0)
+                {
+                    return true;
+                }
+                return false;
+            } catch { return false; }
+            
+        }
+        public bool aendereBenutzer(Benutzer benutzer)
+        {
+            try
+            {
+                if (datenhaltung.updateUser(aktBenutzer.id, benutzer.name, benutzer.passwort))
+                {
+                    aktualisiereBenutzerdaten();
+                    return true;
+                }
+                return false;
+            } catch { return false; }
+        }
+        public bool loescheBenutzer(Benutzer benutzer)
+        {
+            try
+            {
+                bool status = datenhaltung.deleteUser(aktBenutzer.id);
+                schliesseBenutzer();
+                return status;
+            } catch { return false; }
+        }
+
+        // Benutzer-Artikel-Management
+        public bool erzeugeBenutzerArtikel(Artikel artikel)
+        {
+            try
+            {
+                int artikel_id = datenhaltung.insertItem(artikel.name,
+                                                         artikel.kurzbeschr,
+                                                         artikel.langbeschr,
+                                                         artikel.ablaufdatum,
+                                                         artikel.hoechstgebot,
+                                                         0,
+                                                         aktBenutzer.id);
+                if (artikel_id != 0 && datenhaltung.getItem(artikel_id) != null)
+                {
+                    aktualisiereBenutzerdaten();
+                    return true;
+                }
+                return false;
+            } catch { return false; }
+        }
+        public bool aendereBenutzerArtikel(Artikel artikel)
+        {
+            try
+            {
+                if (datenhaltung.updateItem(artikel.id,
+                                            artikel.name,
+                                            artikel.kurzbeschr,
+                                            artikel.langbeschr,
+                                            artikel.ablaufdatum,
+                                            artikel.hoechstgebot,
+                                            artikel.bieter_id,
+                                            aktBenutzer.id))
+                {
+                    aktualisiereBenutzerdaten();
+                    return true;
+                }
+                return false;
+            } catch { return false; }
+        }
+        public bool loescheBenutzerArtikel(Artikel artikel)
+        {
+            try
+            {
+                if (datenhaltung.deleteItem(artikel.id))
+                {
+                    aktualisiereBenutzerdaten();
+                    return true;
+                }
+                return false;
+            } catch { return false; }
+        }
+
+        // Benutzer-Adressen-Management
+        public bool erzeugeBenutzerAdresse(BenutzerAdresse benutzeradresse)
+        {
+            try
+            {
+                Adresse temp_adresse = datenhaltung.getAddress(benutzeradresse.adresse.str_nr,
+                                                               benutzeradresse.adresse.plz,
+                                                               benutzeradresse.adresse.ort,
+                                                               benutzeradresse.adresse.land);
+
+                int adresse_id;
+                if (temp_adresse == null)
+                {
+                    adresse_id = datenhaltung.insertAddress(benutzeradresse.adresse.str_nr,
+                                                            benutzeradresse.adresse.plz,
+                                                            benutzeradresse.adresse.ort,
+                                                            benutzeradresse.adresse.land);
+                }
+                else
+                {
+                    adresse_id = temp_adresse.id;
+                }
+
+                if (datenhaltung.insertUserAddress(benutzeradresse.benutzer_id,
+                                                   adresse_id,
+                                                   benutzeradresse.vname,
+                                                   benutzeradresse.nname,
+                                                   benutzeradresse.addr_zusatz,
+                                                   benutzeradresse.rech_addr,
+                                                   benutzeradresse.lief_addr))
+                {
+                    aktualisiereBenutzerdaten();
+                    return true;
+                }
+                return false;
+            } catch { return false; }
+        }
+        public bool aendereBenutzerAdresse(BenutzerAdresse benutzeradresse)
+        {
+            try
+            {
+                if (datenhaltung.updateUserAddress(benutzeradresse.benutzer_id, 
+                                                   benutzeradresse.adresse.id,
+                                                   benutzeradresse.vname,
+                                                   benutzeradresse.nname,
+                                                   benutzeradresse.addr_zusatz,
+                                                   benutzeradresse.rech_addr,
+                                                   benutzeradresse.lief_addr))
+                {
+                    aktualisiereBenutzerdaten();
+                    return true;
+                }
+                return false;
+            } catch { return false; }
+        }
+        public bool loescheBenutzerAdresse(BenutzerAdresse benutzeradresse)
+        {
+            try
+            {
+                if (datenhaltung.deleteUserAddress(benutzeradresse.benutzer_id, benutzeradresse.adresse.id))
+                {
+                    aktualisiereBenutzerdaten();
+                    return true;
+                }
+                return false;
+            }
+            catch { return false; }
+        }
+
+    }
+}
